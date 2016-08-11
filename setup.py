c3d017d067a9337d67c24414b98a3ef55e9a3c44
Sergey Lushkovsky <s.lushkovsky@gmail.com>
2016-08-12 02:58:15 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='geomtypes',
+    version='0.1',
+    description='Simple geomery types',
+    author='Sergey Lushkovsky',
+    author_email='s.lushkovsky@gmail.com',
+    packages=[],
+)
